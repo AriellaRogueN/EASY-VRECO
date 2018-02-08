d36ef5426b8a7ba9cb53da6874478e7b157d7cf8
AriellaRogueN <arrodriguezn@gmail.com>
2018-02-08 15:59:44 -0300

    agregando comentarios

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,4 @@
+//Inicializando mapa
 function initMap() {
     var chili = {
         lat: -33.418878299999996, lng: -70.64229580000001
@@ -11,16 +12,18 @@ function initMap() {
         map: map
     });
 
-
+//funcion buscar en base a si el usuario acepta geolocalizacion (exito) o hay algún fallo(error).
 function buscar() {
     if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(funcionExito, funcionError); //getCurrentPosition permite al usuario obtener su ubicación actual, uncionExito se ejecuta solo cuando el usuario comparte su ubicación, mientras que funcionError se ejecuta cuando se produce un error en la geolocalización
+        navigator.geolocation.getCurrentPosition(funcionExito, funcionError); 
     }
 }
 
+//Evento al boton para ejecutar funcion buscar
 document.getElementById("encuentrame").addEventListener("click", buscar);
 
-var funcionExito = function (posicion) { //var funcionExito, con el que obtendremos nuestra latitud o longitud y además crearemos un marcador de nuestra ubicación.
+var latitud, longitud;
+var funcionExito = function (posicion) { 
     latitud = posicion.coords.latitude;
     longitud = posicion.coords.longitude;
 
@@ -36,42 +39,36 @@ var funcionExito = function (posicion) { //var funcionExito, con el que obtendre
     });
 }
 
-var funcionError = function (error) { //funcionError con un mensaje para el usuario, en caso de que nuestra geolocalización falle.
+var funcionError = function (error) { 
     alert("tenemos un problema con encontrar tu ubicación");
 }
-
-var inputPartida = document.getElementById("punto-partida")
-var inputDestino = document.getElementById("punto-destino")
+//Autocompletado de inputs.
+var inputPartida = document.getElementById("punto-partida");
+var inputDestino = document.getElementById("punto-destino");
 new google.maps.places.Autocomplete(inputPartida);
 new google.maps.places.Autocomplete(inputDestino);
 
+//Trazar una ruta.
+var directionsService = new google.maps.DirectionsService;
+var directionsDisplay = new google.maps.DirectionsRenderer;
 
-
-var ds = new google.maps.DirectionService;
-var dr = new google.maps.DirectionRenderer;
-
-
-var calculateAndDisplayRoute = function(ds,dr){
-    ds.route({
+var calculateAndDisplayRoute = function(directionsService, directionsDisplay) {
+    directionsService.route({
         origin: inputPartida.value,
         destination: inputDestino.value,
-        travelMode: 'DRIVING'
-    }, function(response, status){
-        if(status === "OK"){
-            ds.setDirections(response);
+        travelMode: "DRIVING"
+    }, function(response, status) {
+        if (status === 'OK') {
+            directionsDisplay.setDirections(response);
         } else {
             window.alert("No encontramos una ruta");
         }
-    })
-}
- 
-ds.setmap(map);
-
-var trazarRuta = function () {
-    calculateAndDisplayRoute(ds,dr);
+    });
+};
+directionsDisplay.setMap(map);
+var trazarRuta = function() {
+    calculateAndDisplayRoute(directionsService, directionsDisplay);
 };
 
-document.getElementById("trazar-ruta").addEventListener("click",trazarRuta);
-
-
+document.getElementById("trazar-ruta").addEventListener("click", trazarRuta);
 };
