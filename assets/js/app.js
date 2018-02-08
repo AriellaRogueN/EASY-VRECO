344d26f502d97ba0577496eab11015ba4ae56bee
tcltere <t.canette@gmail.com>
2018-02-08 17:06:58 -0300

    subiendo mi codigo

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,74 +1,28 @@
-//Inicializando mapa
 function initMap() {
-    var chili = {
-        lat: -33.418878299999996, lng: -70.64229580000001
-    };
-    var map = new google.maps.Map(document.getElementById('map'), {
-        zoom: 4,
-        center: chili
-    });
-    var marker = new google.maps.Marker({
-        position: chili,
-        map: map
-    });
+        var map = new google.maps.Map(document.getElementById('map'), {
+          mapTypeControl: false,
+          center: {lat: -33.8688, lng: 151.2195},
+          zoom: 13
+        });
 
-//funcion buscar en base a si el usuario acepta geolocalizacion (exito) o hay algún fallo(error).
-function buscar() {
-    if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(funcionExito, funcionError); 
-    }
-}
-
-//Evento al boton para ejecutar funcion buscar
-document.getElementById("encuentrame").addEventListener("click", buscar);
-
-var latitud, longitud;
-var funcionExito = function (posicion) { 
-    latitud = posicion.coords.latitude;
-    longitud = posicion.coords.longitude;
-
-    var miUbicacion = new google.maps.Marker({
-        position: {lat: latitud,lng: longitud},
-        map: map
-    });
-
-    map.setZoom(18);
-    map.setCenter({
-        lat: latitud,
-        lng: longitud
-    });
-}
-
-var funcionError = function (error) { 
-    alert("tenemos un problema con encontrar tu ubicación");
-}
-//Autocompletado de inputs.
-var inputPartida = document.getElementById("punto-partida");
-var inputDestino = document.getElementById("punto-destino");
-new google.maps.places.Autocomplete(inputPartida);
-new google.maps.places.Autocomplete(inputDestino);
-
-//Trazar una ruta.
-var directionsService = new google.maps.DirectionsService;
-var directionsDisplay = new google.maps.DirectionsRenderer;
-
-var calculateAndDisplayRoute = function(directionsService, directionsDisplay) {
-    directionsService.route({
-        origin: inputPartida.value,
-        destination: inputDestino.value,
-        travelMode: "DRIVING"
-    }, function(response, status) {
-        if (status === 'OK') {
-            directionsDisplay.setDirections(response);
-        } else {
-            window.alert("No encontramos una ruta");
+        function buscar(){
+          if(navigator.geolocation){
+            navigator.geolocation.getCurrentPosition(functionExito, functionerror);
+          }
         }
-    });
-};
-directionsDisplay.setMap(map);
-var trazarRuta = function() {
-    calculateAndDisplayRoute(directionsService, directionsDisplay);
-};
+        var latitud,longitud;
+        var functionExito = function(posicion){
+          latitud = posicion.coords.latitude;
+          longitud = posicion.coords.longitude;
+        }
+        var miUbicacion = new google.maps.Marker({
+          position: {lat:latitud, lng:longitud},
+          map: map
+        });
+        var functionError = function(error){
+          alert("tenemos un problema al encontrar tu ubicacion");
+          document.getElementById("encuentrame").addEventListener("click")
+        }
+        var inputPartida = document.getElementById
+      }
 
-document.getElementById("trazar-ruta").addEventListener("click", trazarRuta);
-};
